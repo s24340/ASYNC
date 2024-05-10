cf4e083d591202cbbc4649be53438a3f48e17699
s24340 <s24340@pjwstk.edu.pl>
2024-05-10 20:10:51 +0200

    required

--- async/Models/Warehouse.cs
+++ async/Models/Warehouse.cs
@@ -4,11 +4,15 @@ namespace async.Models;
 
 public abstract class Warehouse
 {
+    [Required]
     public int IdProduct { get; set; }
     
+    [Required]
     public int IdWarehouse { get; set; }
     
+    [Required]
     public int Amount { get; set; }
     
+    [Required]
     public string CreatedAt { get; set; }
 }
